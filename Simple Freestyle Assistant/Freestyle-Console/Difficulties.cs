2f7a1fb46571776889a881c57895ba9445b35fdb
CruXial <benjamin@theones.dk>
2017-11-23 16:25:39 +0100

    Added a program
    -Added "Simple Freestyle Assistant"

---
+++ Simple Freestyle Assistant/Freestyle-Console/Difficulties.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Freestyle.Difficulties
+{
+    public class Difficulties
+    {
+        public static void FreestyleEz()
+        {
+            Random random = new Random();
+
+            switch (random.Next(1, 6 + 1))
+            {
+                case 1:
+                    Console.WriteLine("Bomb");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Plane");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Line");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Lake");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Shore");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Bowling");
+                    break;
+            }
+        }
+
+        public static void FreestyleMed()
+        {
+            Random random = new Random();
+
+            switch (random.Next(1, 6 + 1))
+            {
+                case 1:
+                    Console.WriteLine("School");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Shot");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Skate");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Drone");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Set");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Code");
+                    break;
+            }
+        }
+
+        public static void FreestyleHard()
+        {
+            Random random = new Random();
+
+            switch (random.Next(1, 6 + 1))
+            {
+                case 1:
+                    Console.WriteLine("Honey");
+                    break;
+
+                case 2:
+                    Console.WriteLine("University");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Neighbour");
+                    break;
+
+                case 4:
+                    Console.WriteLine("City");
+                    break;
+
+                case 5:
+                    Console.WriteLine("Alaska");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Bastard");
+                    break;
+            }
+        }
+    }
+}
---
+++ Simple Freestyle Assistant/Freestyle-Console/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Freestyle.Difficulties;
+
+namespace Freestyle_Console
+{
+    class Program : Difficulties
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("CruXial's simple freestyle assistant v 1.0 \n \n");
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Hello there and welcome, to procced press <ENTER>");
+
+            if(Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("\n");
+                return;
+            }
+            
+            Console.WriteLine("\nType one of the following difficulties: \n Easy \n Medium \n Hard\n\n");
+
+            Console.Write("Your preferred difficulty here: ");
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            var readLine = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            if (readLine.ToLower() == "easy")
+            {
+                Console.WriteLine("\n");
+                while (true)
+                {
+                    FreestyleEz();
+                    Thread.Sleep(3000);
+                }
+            }
+
+            if (readLine.ToLower() == "medium")
+            {
+                Console.WriteLine("\n");
+                while (true)
+                {
+                    FreestyleMed();
+                    Thread.Sleep(3000);
+                }
+            }
+
+            if (readLine.ToLower() == "hard")
+            {
+                Console.WriteLine("\n");
+                while (true)
+                {
+                    FreestyleHard();
+                    Thread.Sleep(3000);
+                }
+            }
+
+            return;
+        }
+    }
+}
